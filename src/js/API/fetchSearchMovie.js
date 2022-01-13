7a09fbd5eba6a290ed7bfd1926a8c681f89c0a65
Sofia Protsiv <prochivsofia@gmail.com>
2022-01-13 13:45:25 +0200

    setItem currentpage in LS

---
+++ src/js/API/fetchSearchMovie.js
@@ -0,0 +1,29 @@
+export default class FetchSearchMovie {
+    constructor() {
+        this.BASE_URL = 'https://api.themoviedb.org/3'
+        this.API_KEY = '208491dbcc6d03ee351feb599226bf58',
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    async fetchMovies() {
+        const url = `${this.BASE_URL}/search/movie?api_key=${this.API_KEY}&query=${this.searchQuery}&page=${this.page}`;
+        return await fetch(url)
+        .then(res => {
+            this.incrementPage();
+            return res.json();
+        })
+    }
+    incrementPage() {
+        this.page += 1;
+    }
+    resetPage() {
+        this.page = 1;
+    }
+    get query() {
+        return this.searchQuery.trim();
+    }
+
+    set query(value) {
+        this.searchQuery = value;
+    }
+};
--- src/js/scripts/refs.js
+++ src/js/scripts/refs.js
@@ -1,10 +1,20 @@
 const refs = {
     // header section elements
+    formEl: document.querySelector('.search-form'),
+    inputEl: document.querySelector('.search-form__input'),
 
     // gallary section elements
+    mainEl: document.querySelector('main'),
+    galleryEl: document.querySelector('.movies'),
+    loadMoreBtnEl: document.querySelector('.loadMoreBtn'),
+    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+
+    // pagination section elements
 
     // footer section elements
-    footerTeamLinkEl: document.querySelector('.footer__link'),
+    footerLinkEl: document.querySelector('.footer__link'),
+    footerTextEl: document.querySelector('.footer__text--first'),
+    footerEl: document.querySelector('.footer__text--second'),
 };
 
 export { refs };
