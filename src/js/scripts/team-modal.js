51eae425f1805f4504b9f330875ddd1df4b24b94
Lelyak-Andriy <a.lelyak47@gmail.com>
2022-01-15 14:40:32 +0200

    2

---
+++ src/js/scripts/team-modal.js
@@ -0,0 +1,62 @@
+// import js
+import { refs } from './refs';
+
+// import templates
+import movieCard from '../templates/modal.hbs';
+
+refs.openModalEl.addEventListener('click', onOpenModal);
+refs.backdropEl.addEventListener('click', onBackdropClick);
+
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    onCloseModal();
+  }
+}
+
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
+  refs.bodyEl.classList.remove('show-modal');
+  refs.closeModalEl.removeEventListener('click', onCloseModal);
+  refs.modalEl.classList.add('js-backdrop');
+}
+
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
+}
+
+function onOpenModal(e) {
+  e.preventDefault();
+
+  window.addEventListener('keydown', onEscKeyPress);
+  refs.closeModalEl.addEventListener('click', onCloseModal);
+
+  if (e.target.classList.value === 'movies') {
+  return
+}
+
+  const currentFilmId = Number(e.target.closest('li').id);
+  
+ return JSON.parse(localStorage.getItem("currentPage")).map(films => {
+   films.forEach(film => {
+  
+     if (currentFilmId === film.id ) {
+        
+      const markupModal = movieCard(film);
+      
+      refs.modalmarkupEl.innerHTML = '';
+      refs.modalmarkupEl.insertAdjacentHTML('beforeend', markupModal);
+      refs.bodyEl.classList.add('show-modal');
+     }  
+
+     })
+  })
+
+}
