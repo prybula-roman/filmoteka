b856ef47325d51e34b52aacd76076d3e0f05c1cc
Lelyak-Andriy <a.lelyak47@gmail.com>
2022-01-17 23:00:49 +0200

    sorting by genre

--- src/js/scripts/filter.js
+++ src/js/scripts/filter.js
@@ -3,14 +3,14 @@ import handleMovieCard from './handleMovieCard';
 import filmCard from '../templates/preview_card.hbs';
 import { onRenderPagination } from './pagination'
 
-class MovieFilter {
+class MovieGenreFilter {
     constructor() {
       this.BASE_URL = 'https://api.themoviedb.org/3'
       this.API_KEY = '208491dbcc6d03ee351feb599226bf58',
       this.page = 1;
     }
-    async fetchMovies(sort) {
-      const url = `${this.BASE_URL}/discover/movie?&sort_by=${sort}&api_key=${this.API_KEY}&page=${this._page}&language=en-US`;
+    async fetchMovies(genre) {
+      const url = `${this.BASE_URL}/discover/movie?&sort_by=${genre}&api_key=${this.API_KEY}&page=${this._page}&language=en-US`;
       return await fetch(url)
           .then(response => (response.ok ? response.json() : []))
           .catch(error => console.log(error));
@@ -34,22 +34,22 @@ class MovieFilter {
   }
 
 
-const movieFilter = new MovieFilter();
+const moviesFilter = new MovieGenreFilter();
 
 
 let sortValue = '';
 
-document.querySelectorAll('.filter-input').forEach(item => {
+document.querySelectorAll('.genre-input').forEach(item => {
   item.addEventListener('change', event => {
-    movieFilter.resetPage();
+    moviesFilter.resetPage();
     refs.formEl.value = '';
     sortValue = document.querySelector('#sortpicker').value;
     createCard(sortValue);
   });
 });
 
-function createCard(sort) {
-  movieFilter.fetchMovies(sort).then(res => {
+function createCard(genre) {
+  moviesFilter.fetchMovies(genre).then(res => {
     refs.galleryEl.innerHTML = filmCard(handleMovieCard(res.results));   
     refs.paginationEl.innerHTML = ''    
     if(res.total_pages >= 500){
