080ea8bc98fd51490d97f4a29fda467608dfc87e
Sofia Protsiv <prochivsofia@gmail.com>
2022-01-18 21:08:50 +0200

    add sort and fix gallery

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './sass/main.scss';
 
 import './js/scripts/search';
 
-import './js/scripts/library'
+import './js/scripts/library';
 
 import './js/scripts/pagination';
 
@@ -12,6 +12,8 @@ import './js/scripts/modal';
 
 import './js/scripts/scrollToTop';
 
+import './js/scripts/sort';
+
 import './js/scripts/toggle-theme';
 
-import './js/scripts/header'
+//import './js/scripts/header';
